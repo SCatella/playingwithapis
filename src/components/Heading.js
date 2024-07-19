82cbe5f49d5bc02b9f49b59fc2e5adfeccce1b4d
SCatella <samuel.catella@gmail.com>
2024-07-19 13:16:28 -0600

    initial commit + added a heading component

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Heading.css";
+
+const Heading = () => {
+  return (
+    <div>
+      <h1>playing with API's</h1>
+    </div>
+  )
+}
+
+export default Heading;
---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Heading from '../components/Heading'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Heading />
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
